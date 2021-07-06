4a15733900bf0002937528ee2f40af61c941dffc
Mariem Jomaa <Mariem.Jomaa@lombardrisk.com>
2021-07-06 23:55:23 +0100

    feat (pi) : add trigger on table user

---
+++ PI-backend/src/Entity/Personne.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PersonneRepository::class)
+ */
+class Personne
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+ 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+
+
+}
